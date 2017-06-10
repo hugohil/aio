5dfbe53543d582c31baebbe65e3bc21a2f617e03
hugohil <hilairehugo@gmail.com>
2017-06-10 17:31:04 +0200

    Basic nd setup

---
+++ src/Root.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="root">
+    <counter></counter>
+    <p>
+      Welcome to neodymium!
+    </p>
+    <p>
+      To get a better understanding of how this works, check out
+      <a href="http://soixantecircuits.github.io/nd" target="_blank" :style="'color: ' + color">its documentation</a>.
+      It is also recommended to go through the docs for
+      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
+      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
+      If you have any issues with the setup, please file an issue at this boilerplate's
+      <a href="https://github.com/soixantecircuits/nd" target="_blank">repository</a>.
+    </p>
+  </div>
+</template>
+
+<script>
+'use strict'
+
+import Counter from './components/Counter'
+var settings = require('electron').remote.getGlobal('settings') // Global variable set in mainProcess
+
+export default {
+  data() {
+    return {
+      color: settings.color || '#42b983'
+    }
+  },
+  components: {
+    Counter
+  },
+  mounted() {
+    this.$store.state.count = settings.count || 0
+  }
+}
+</script>
+<style>
+html {
+  height: 100%;
+  font-family: Source Sans Pro, Helvetica, sans-serif;
+}
+
+#app {
+  color: #2c3e50;
+  max-width: 600px;
+  text-align: center;
+}
+
+#app a {
+  color: #42b983;
+  text-decoration: none;
+}
+#app p {
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 600px;
+}
+
+#app > div {
+  position: absolute;
+  width: 100%;
+}
+</style>
---
+++ src/vuex/store.js
@@ -0,0 +1,26 @@
+'use strict'
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state = {
+  count: 0
+}
+
+const mutations = {
+  INCREMENT (state) {
+    state.count++
+  },
+  DECREMENT (state) {
+    state.count--
+  }
+}
+
+const store = new Vuex.Store({
+  state,
+  mutations
+})
+
+export default store
