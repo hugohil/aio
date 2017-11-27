1211e182b8d2313967f3279a279cb21bf2a46600
hugohil <mail@hugohil.online>
2017-11-27 15:00:59 +0100

    Basic soixantecircuits/electron-vue setup

---
+++ src/renderer/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'aio'
+  }
+</script>
+
+<style>
+  @import './style/css/reset';
+  @import './style/css/kiosk';
+  /* CSS */
+</style>
---
+++ src/renderer/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="wrapper">
+    <main></main>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'home',
+    methods: {}
+  }
+</script>
+
+<style></style>
